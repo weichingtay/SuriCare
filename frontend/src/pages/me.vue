936b5933b572a24e36e0a0ad4efad7c1aec32ed1
Tay Wei Ching <87612522+weichingtay@users.noreply.github.com>
2025-06-02 16:25:03 +0800

    update

--- frontend/src/pages/me.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div><h1>This is my first page.</h1></div>   
-     <div><h1>This is my changes.</h1></div>
-     <div><h1>This is my second changes.</h1></div>
-          <div><h1>This is my third changes.</h1></div>
-
-
-</template>
