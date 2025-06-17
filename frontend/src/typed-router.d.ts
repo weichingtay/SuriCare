177b631ad53904fde8d241a1a9fc4755bd47f766
ongks-useR <ks.ong86@me.com>
2025-06-17 14:09:35 +0800

    update backend codes

--- frontend/src/typed-router.d.ts
+++ frontend/src/typed-router.d.ts
@@ -23,5 +23,6 @@ declare module 'vue-router/auto-routes' {
     '/checkin': RouteRecordInfo<'/checkin', '/checkin', Record<never, never>, Record<never, never>>,
     '/guidance': RouteRecordInfo<'/guidance', '/guidance', Record<never, never>, Record<never, never>>,
     '/login': RouteRecordInfo<'/login', '/login', Record<never, never>, Record<never, never>>,
+    '/dashboard': RouteRecordInfo<'/dashboard', '/dashboard', Record<never, never>, Record<never, never>>,
   }
 }
