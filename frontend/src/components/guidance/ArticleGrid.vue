07561860777c7b7ef56ad766a903f194b66b6fb5
king-446 <zamil121297@gmail.com>
2025-07-01 15:02:26 +0800

    style: guidance

--- frontend/src/components/guidance/ArticleGrid.vue
+++ frontend/src/components/guidance/ArticleGrid.vue
@@ -1,20 +1,39 @@
 <template>
-  <v-row>
-    <v-col
-      v-for="article in articles"
-      :key="article.id"
-      cols="12"
-      md="4"
-      sm="6"
+  <div>
+    <div
+      v-if="articles.length === 0"
+      class="text-center py-12"
     >
-      <ArticleCard :article="article" />
-    </v-col>
-  </v-row>
+      <v-icon
+        class="mb-4"
+        color="grey-lighten-2"
+        size="64"
+      >
+        mdi-book-outline
+      </v-icon>
+      <h3 class="text-h6 text-grey-darken-1 mb-2">No articles available</h3>
+      <p class="text-body-2 text-grey-darken-1">
+        Please check back later or contact support if you need help.
+      </p>
+    </div>
+
+    <v-row>
+      <v-col
+        v-for="article in articles"
+        :key="article.id"
+        cols="12"
+        md="4"
+        sm="6"
+      >
+        <ArticleCard :article="article" />
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script setup lang="ts">
-  import { ref } from 'vue';
-  import ArticleCard from './ArticleCard.vue';
+  import { ref } from 'vue'
+  import ArticleCard from './ArticleCard.vue'
 
   const articles = ref([
     {
@@ -80,5 +99,5 @@
         'Lorem Ipsum dolor sit amet consectetur. Eu fames et faucibus gravida est non mi massa lacus. Lorem ipsum dolor sit amet consectetur.',
       tags: ['Sleep', 'Sleeping'],
     },
-  ]);
+  ])
 </script>
